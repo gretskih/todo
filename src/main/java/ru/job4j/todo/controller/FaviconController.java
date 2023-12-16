40ad6a3200b7568084ac9c9f3152bd94791bfc19
AGretskikhHome <gretskih@mail.ru>
2023-12-16 23:40:46 +0300

    Переделан дизайн видов, добавлено описание в README.md. Вторая версия приложения v.2

---
+++ src/main/java/ru/job4j/todo/controller/FaviconController.java
@@ -0,0 +1,13 @@
+package ru.job4j.todo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FaviconController {
+    @GetMapping("favicon.ico")
+    @ResponseBody
+    void returnNoFavicon() {
+    }
+}
--- src/main/java/ru/job4j/todo/controller/IndexController.java
+++ src/main/java/ru/job4j/todo/controller/IndexController.java
@@ -5,7 +5,6 @@ import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import ru.job4j.todo.model.Task;
 import ru.job4j.todo.service.TaskService;
 
@@ -105,4 +104,5 @@ public class IndexController {
         }
         return "redirect:/index";
     }
+
 }
