53e6cceb009be0b4cd76bbe27e0b83984db4a389
AGretskikhHome <gretskih@mail.ru>
2023-12-20 22:50:36 +0300

    ToOne

--- src/main/java/ru/job4j/todo/controller/TaskController.java
+++ src/main/java/ru/job4j/todo/controller/TaskController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.job4j.todo.model.Task;
+import ru.job4j.todo.model.User;
 import ru.job4j.todo.service.TaskService;
 
 @ThreadSafe
--- src/main/java/ru/job4j/todo/model/Task.java
+++ src/main/java/ru/job4j/todo/model/Task.java
@@ -20,4 +20,7 @@ public class Task {
     private String description;
     private LocalDateTime created = LocalDateTime.now();
     private boolean done;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
